62020e054afef373a3c17358182b7fe92826d75a
Makarenko Ivan <ivan.makarenko@free.fr>
2022-10-02 19:20:00 +0200

    routes navigation

---
+++ src/routes/administration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './administration.css';
+
+const administration = () => {
+  return (
+    <>
+      <h2>administration</h2>
+    </>
+  )
+}
+
+export default administration
---
+++ src/routes/entrainement.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './entrainement.css';
+
+const entrainement = () => {
+  return (
+    <>
+      <h2>entrainement</h2>
+    </>
+  )
+}
+
+export default entrainement
